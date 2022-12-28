ca5774c6442e1e99cf5003a36343fde4618319b7
AnyaSE <annseleznova@gmail.com>
2022-12-28 11:31:32 +0800

    completed js code for HW7

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 const arrayOne = ['Milan', 'London', 'Warsaw', 'Kyiv', 'Odessa', 'New York'];
 const arrayTwo = ['Beijin', 'Singapore', 'Warsaw', 'Odessa', 'Astana'];
 
+const newJointArray = arrayOne.concat(arrayTwo); 
+
+function uniqueElements(array){
+    const uniqueArray = [];
+    if (Array.isArray(arrayOne) && Array.isArray(arrayTwo)) {
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            if(!(uniqueArray.includes(element))){
+                uniqueArray.push(element);
+            }
+        }
+    } else {
+        console.log ('The input is not an array. Please try again');
+    }
+    return uniqueArray;
+}; 
+
+console.log(uniqueElements(newJointArray));
