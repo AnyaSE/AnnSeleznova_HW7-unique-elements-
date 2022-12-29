341f65986361e6089f292b8fa5ddf50ffbd192e2
AnyaSE <annseleznova@gmail.com>
2022-12-29 10:15:06 +0800

    corrected homework No.7

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 const arrayOne = ['Milan', 'London', 'Warsaw', 'Kyiv', 'Odessa', 'New York'];
 const arrayTwo = ['Beijin', 'Singapore', 'Warsaw', 'Odessa', 'Astana'];
 
-const newJointArray = arrayOne.concat(arrayTwo); 
+const uniqueArray = [];
 
 function uniqueElements(array){
-    const uniqueArray = [];
-    if (Array.isArray(arrayOne) && Array.isArray(arrayTwo)) {
-        for (let i = 0; i < array.length; i++) {
-            const element = array[i];
-            if(!(uniqueArray.includes(element))){
+    if (Array.isArray(arrayOne) && Array.isArray(arrayTwo)){
+        for (let i = 0; i < arrayOne.length; i++) {
+            const element = arrayOne [i];
+            if(!(arrayTwo.includes(element))){
                 uniqueArray.push(element);
+            }
+        }
+        for (let b = 0; b < arrayTwo.length; b++) {
+            const number = arrayTwo [b];
+            if(!(arrayOne.includes(number))){
+                uniqueArray.push(number);
             }
         }
     } else {
         console.log ('The input is not an array. Please try again');
     }
     return uniqueArray;
-}; 
+};
 
-console.log(uniqueElements(newJointArray));
+console.log(uniqueElements(uniqueArray));
